c6f5e2822535b6e57a0c638b7f9d83c356b4fc73
baizuige <baizuige@gmail.com>
2022-04-11 11:23:09 +0800

    feat: 添加依赖

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="">
+        <div class="admin-login">
+            <img src="~assets/img/logo.png" class="login-logo" />
+            <div class="login-box">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.login {
+    height: 100%;
+    width: 100%;
+    background: url('../assets/img/login-bg.jpg') no-repeat;
+    background-size: 100% 100%;
+    display: flex;
+    justify-content: center;
+    .admin-login {
+        width: 960px;
+        padding-top: 20px;
+    }
+}
+</style>
